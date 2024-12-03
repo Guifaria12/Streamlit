61cf0e2b61a9804d4ebba44a3b3f1a1e05be8d26
Guifaria12 <gui.faria69@gmail.com>
2024-12-02 23:28:47 -0300

    Update testefinal.app.py

--- testefinal.app.py
+++ testefinal.app.py
@@ -113,9 +113,9 @@ fig.update_layout(
 # Exibir o gráfico interativo no Streamlit
 st.plotly_chart(fig)
 
-st.header("**Eventos que impactaram os preços:**")
+st.subheader("**Eventos que impactaram os preços:**")
 
-st.subheader("""
+st.writer("""
  * 1990-1991: Guerra do Golfo: Conflito no Oriente Médio elevou brevemente os preços, mas a recessão global limitou o impacto.
  * 2001: Ataques de 11 de Setembro: Aumento temporário nos preços e foco na segurança energética dos EUA.
  * 2003: Invasão do Iraque: Instabilidade geopolítica impulsionou preços durante crescimento global acelerado.
