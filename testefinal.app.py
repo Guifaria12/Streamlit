6cbf8900fb56b1c7f705951e7f617abfc049a50c
Guifaria12 <gui.faria69@gmail.com>
2024-12-03 19:41:59 -0300

    Update testefinal.app.py

--- testefinal.app.py
+++ testefinal.app.py
@@ -26,6 +26,12 @@ df_matriz_energica['Petróleo'] = df_matriz_energica['Petróleo'].str.replace('%
 # Filtrar dados após o ano de 1987
 df_matriz_energica = df_matriz_energica.loc[df_matriz_energica['Ano'] >= '1987']
 
+st.subheader ("""
+**Faculdade de Informática e Administração Paulista – FIAP**
+
+**Grupo 47: Cristiano de Araujo Santos Filho, Eduardo Vilela Silva, Guilherme de Faria
+Rodrigues, Marcelo Pereira Varesi e Vitória Pinheiro de Mattos**
+""")
 st.header("**Introdução**")
 
 st.write("""
